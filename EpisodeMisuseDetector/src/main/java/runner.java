5f56103181f55dda6a91bf9823045f1b2265a0a3
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-05-17 16:52:27 +0200

    Add integration with MUDetect detection.

--- EpisodeMisuseDetector/src/main/java/runner.java
+++ EpisodeMisuseDetector/src/main/java/runner.java
@@ -1,15 +1,27 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import cc.episodeMining.mudetect.EpisodesToPatternTransformer;
+import cc.episodeMining.mudetect.FilterAndRankingStrategy;
+import cc.episodeMining.mudetect.TraceToAUGTransformer;
+import cc.kave.episodes.model.Episode;
+import de.tu_darmstadt.stg.mubench.DefaultFilterAndRankingStrategy;
+import de.tu_darmstadt.stg.mubench.ViolationUtils;
+import de.tu_darmstadt.stg.mudetect.MissingElementViolationPredicate;
+import de.tu_darmstadt.stg.mudetect.MuDetect;
+import de.tu_darmstadt.stg.mudetect.aug.model.APIUsageExample;
+import de.tu_darmstadt.stg.mudetect.aug.model.patterns.APIUsagePattern;
+import de.tu_darmstadt.stg.mudetect.mining.MinPatternActionsModel;
+import de.tu_darmstadt.stg.mudetect.model.Violation;
+import de.tu_darmstadt.stg.mudetect.overlapsfinder.AlternativeMappingsOverlapsFinder;
+import de.tu_darmstadt.stg.mudetect.ranking.*;
+import edu.iastate.cs.mudetect.mining.MinPatternActionsModel;
 import org.apache.commons.io.FileUtils;
 
 import cc.episodeMining.data.StreamGenerator;
 import cc.kave.commons.utils.json.JsonUtils;
 import cc.kave.episodes.io.EpisodeParser;
-import cc.kave.episodes.mining.graphs.EpisodeToGraphConverter;
 import cc.kave.episodes.mining.patterns.PatternFilter;
 import cc.kave.episodes.model.EventStream;
 import cc.kave.episodes.model.events.Event;
@@ -18,15 +30,13 @@ import cc.kave.episodes.postprocessor.TransClosedEpisodes;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.inject.Inject;
-import com.mxgraph.util.svg.Parser;
 
 import de.tu_darmstadt.stg.mubench.cli.DetectionStrategy;
 import de.tu_darmstadt.stg.mubench.cli.DetectorArgs;
-import de.tu_darmstadt.stg.mubench.cli.DetectorFinding;
 import de.tu_darmstadt.stg.mubench.cli.DetectorOutput;
 import de.tu_darmstadt.stg.mubench.cli.DetectorOutput.Builder;
 import de.tu_darmstadt.stg.mubench.cli.MuBenchRunner;
+import org.apache.commons.lang.NotImplementedException;
 
 public class runner {
 
@@ -43,17 +53,53 @@ public class runner {
 		public DetectorOutput detectViolations(DetectorArgs args, Builder output)
 				throws Exception {
 			parser(args.getTargetSrcPaths(), args.getDependencyClassPath());
-			return output.withFindings(new ArrayList<DetectorFinding>());
+
+			List<Event> mapping = loadMapping(getMapPath());
+			Set<Episode> episodes = mineEpisodes(getStreamPath());
+            Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer().transform(episodes, mapping);
+
+            Collection<APIUsageExample> targets = loadTargetAUGs(args.getTargetSrcPaths(), args.getDependencyClassPath());
+            MuDetect detection = new MuDetect(
+                    new MinPatternActionsModel(() -> patterns, 2),
+                    new AlternativeMappingsOverlapsFinder(
+                            new AlternativeMappingsOverlapsFinder.Config()
+                    ),
+                    new MissingElementViolationPredicate(),
+                    new DefaultFilterAndRankingStrategy(new WeightRankingStrategy(
+                            new ProductWeightFunction(
+                                    new OverlapWithoutEdgesToMissingNodesWeightFunction(new ConstantNodeWeightFunction()),
+                                    new PatternSupportWeightFunction(),
+                                    new ViolationSupportWeightFunction()
+                            ))));
+            List<Violation> violations = detection.findViolations(targets);
+
+            return output.withFindings(violations, ViolationUtils::toFinding);
+		}
+
+        private Collection<APIUsageExample> loadTargetAUGs(String[] srcPaths, String[] classpath) {
+		    // TODO it is weird that building traces returns a List<Event>, isn't a List<Event> _one_ trace and the type
+            // should be Collection<List<Events>>? I'm assuming this for the subsequent implementation. -Sven
+            Collection<List<Event>> traces = Arrays.asList(buildMethodTraces(srcPaths, classpath));
+
+            Collection<APIUsageExample> targets = new ArrayList<>();
+            for (List<Event> trace : traces) {
+                targets.add(TraceToAUGTransformer.transform(trace));
+            }
+
+            return targets;
+        }
+
+        private Set<Episode> mineEpisodes(File streamPath) {
+			throw new NotImplementedException();
+		}
+
+		private List<Event> loadMapping(File mapPath) {
+			throw new NotImplementedException();
 		}
 
 		public void parser(String[] srcPaths, String[] classpaths)
 				throws IOException {
-			List<Event> sequences = Lists.newLinkedList();
-			for (String srcPath : srcPaths) {
-				StreamGenerator generator = new StreamGenerator();
-				sequences.addAll(generator.generateMethodTraces(new File(
-						srcPath), classpaths));
-			}
+            List<Event> sequences = buildMethodTraces(srcPaths, classpaths);
 
 			Map<Event, Integer> freqEvents = frequentEvents(sequences);
 			EventStream es = new EventStream();
@@ -70,7 +116,17 @@ public class runner {
 			JsonUtils.toJson(es.getMapping(), getMapPath());
 		}
 
-		private Map<Event, Integer> frequentEvents(List<Event> events) {
+        private List<Event> buildMethodTraces(String[] srcPaths, String[] classpaths) {
+            List<Event> sequences = Lists.newLinkedList();
+            for (String srcPath : srcPaths) {
+                StreamGenerator generator = new StreamGenerator();
+                sequences.addAll(generator.generateMethodTraces(new File(
+                        srcPath), classpaths));
+            }
+            return sequences;
+        }
+
+        private Map<Event, Integer> frequentEvents(List<Event> events) {
 			Map<Event, Integer> results = Maps.newLinkedHashMap();
 
 			for (Event e : events) {
