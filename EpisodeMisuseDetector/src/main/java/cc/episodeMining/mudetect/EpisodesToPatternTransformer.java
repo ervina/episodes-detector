efc067bfe733a095d0eae9abf6328596c6202eca
ervina <ervinacergani@ervinas-mbp>
2018-06-11 07:51:11 +0200

    added APIUsages for disconnected nodes

--- EpisodeMisuseDetector/src/main/java/cc/episodeMining/mudetect/EpisodesToPatternTransformer.java
+++ EpisodeMisuseDetector/src/main/java/cc/episodeMining/mudetect/EpisodesToPatternTransformer.java
@@ -5,43 +5,195 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 import cc.kave.commons.model.naming.codeelements.IMethodName;
 import cc.kave.episodes.model.Episode;
 import cc.kave.episodes.model.events.Event;
 import cc.kave.episodes.model.events.Fact;
 import cc.recommenders.datastructures.Tuple;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
 import de.tu_darmstadt.stg.mudetect.aug.model.Node;
 import de.tu_darmstadt.stg.mudetect.aug.model.actions.MethodCallNode;
 import de.tu_darmstadt.stg.mudetect.aug.model.controlflow.OrderEdge;
 import de.tu_darmstadt.stg.mudetect.aug.model.patterns.APIUsagePattern;
 
 public class EpisodesToPatternTransformer {
-    public Set<APIUsagePattern> transform(Set<Episode> episodes, List<Event> mapping) {
-        Set<APIUsagePattern> patterns = new HashSet<>();
-        for (Episode episode : episodes) {
-            patterns.add(transform(episode, mapping));
-        }
-        return patterns;
-    }
+	public Set<APIUsagePattern> transform(Set<Episode> episodes,
+			List<Event> mapping) {
+		Set<APIUsagePattern> patterns = new HashSet<>();
+		for (Episode episode : episodes) {
+			if (containsDisconnectedNodes(episode)) {
+				Set<Episode> genEpisode = generateAllPossible(episode);
+				for (Episode ep : genEpisode) {
+					patterns.add(transform(ep, mapping));
+				}
+			} else {
+				patterns.add(transform(episode, mapping));
+			}
+		}
+		return patterns;
+	}
 
-    private APIUsagePattern transform(Episode episode, List<Event> mapping) {
-        APIUsagePattern pattern = new APIUsagePattern(episode.getFrequency(), new HashSet<>());
-        Map<Integer, Node> factIdToNodeMap = new HashMap<>();
-        for (Fact fact : episode.getFacts()) {
-            if (!fact.isRelation()) {
-                Event event = mapping.get(fact.getFactID());
-                IMethodName method = event.getMethod();
-                MethodCallNode node = TransformerUtils.createCallNode(method);
-                pattern.addVertex(node);
-                factIdToNodeMap.put(fact.getFactID(), node);
-            } else {
-                Tuple<Fact, Fact> relation = fact.getRelationFacts();
-                Node sourceNode = factIdToNodeMap.get(relation.getFirst().getFactID());
-                Node targetNode = factIdToNodeMap.get(relation.getSecond().getFactID());
-                pattern.addEdge(sourceNode, targetNode, new OrderEdge(sourceNode, targetNode));
-            }
-        }
-        return pattern;
-    }
+	private Set<Episode> generateAllPossible(Episode episode) {
+		List<Set<Fact>> connectedFacts = getConnectedNodes(episode);
+		Set<Fact> disconnectedFacts = getDisconnectedNodes(episode,
+				connectedFacts);
+		List<Fact> discFacts = convertToList(disconnectedFacts);
+		Set<Episode> generatedEpisodes = Sets.newLinkedHashSet();
+
+		if (connectedFacts.isEmpty()) {
+			generatedEpisodes.addAll(generateEpisodes(episode, discFacts));
+		}
+		return generatedEpisodes;
+	}
+
+	private List<Fact> convertToList(Set<Fact> set) {
+		List<Fact> list = Lists.newLinkedList();
+		for (Fact fact : set) {
+			list.add(fact);
+		}
+		return list;
+	}
+
+	private Set<Episode> generateEpisodes(Episode episode,
+			List<Fact> disconnected) {
+		Set<Episode> episodes = Sets.newLinkedHashSet();
+
+		for (int idx1 = 0; idx1 < disconnected.size(); idx1++) {
+			Set<Fact> relations1 = Sets.newLinkedHashSet();
+			Set<Fact> relations2 = Sets.newLinkedHashSet();
+
+			for (int idx2 = 0; idx2 < disconnected.size(); idx2++) {
+				if (idx1 != idx2) {
+					relations1.add(new Fact(new Fact(idx1), new Fact(idx2)));
+					relations2.add(new Fact(new Fact(idx2), new Fact(idx1)));
+				}
+			}
+			episodes.add(createEpisode(episode, relations1));
+			episodes.add(createEpisode(episode, relations2));
+		}
+		return episodes;
+	}
+
+	private Episode createEpisode(Episode episode, Set<Fact> relations) {
+		Episode newEpisode = new Episode();
+		newEpisode.setFrequency(episode.getFrequency());
+		newEpisode.setEntropy(episode.getEntropy());
+
+		for (Fact event : episode.getEvents()) {
+			newEpisode.addFact(event);
+		}
+		for (Fact relation : relations) {
+			newEpisode.addFact(relation);
+		}
+		return newEpisode;
+	}
+
+	private Set<Fact> getDisconnectedNodes(Episode episode,
+			List<Set<Fact>> connectedFacts) {
+		Set<Fact> disconnected = Sets.newLinkedHashSet();
+		Set<Fact> connected = convertToSet(connectedFacts);
+
+		for (Fact fact : episode.getEvents()) {
+			if (!connected.contains(fact)) {
+				disconnected.add(fact);
+			}
+		}
+		return disconnected;
+	}
+
+	private Set<Fact> convertToSet(List<Set<Fact>> connectedFacts) {
+		Set<Fact> facts = Sets.newLinkedHashSet();
+		for (Set<Fact> set : connectedFacts) {
+			facts.addAll(set);
+		}
+		return facts;
+	}
+
+	private List<Set<Fact>> getConnectedNodes(Episode episode) {
+		List<Set<Fact>> connectedNodes = Lists.newLinkedList();
+		Map<Fact, Integer> firstFacts = getFactsAsFirstNodeInRelation(episode);
+		TreeSet<Integer> sortedFirsts = sortFirstFacts(firstFacts);
+
+		for (Integer value : sortedFirsts.descendingSet()) {
+			Set<Fact> facts = Sets.newLinkedHashSet();
+
+			for (Map.Entry<Fact, Integer> entry : firstFacts.entrySet()) {
+				if (entry.getValue() == value) {
+					facts.add(entry.getKey());
+				}
+			}
+			connectedNodes.add(facts);
+		}
+		return connectedNodes;
+	}
+
+	private TreeSet<Integer> sortFirstFacts(Map<Fact, Integer> factCounter) {
+		TreeSet<Integer> sort = Sets.newTreeSet();
+		for (Map.Entry<Fact, Integer> entry : factCounter.entrySet()) {
+			sort.add(entry.getValue());
+		}
+		return sort;
+	}
+
+	private Map<Fact, Integer> getFactsAsFirstNodeInRelation(Episode episode) {
+		Map<Fact, Integer> firstFacts = Maps.newLinkedHashMap();
+
+		for (Fact relation : episode.getRelations()) {
+			Tuple<Fact, Fact> tuple = relation.getRelationFacts();
+			Fact first = tuple.getFirst();
+			Fact second = tuple.getSecond();
+			if (firstFacts.containsKey(first)) {
+				int counter = firstFacts.get(first);
+				firstFacts.put(first, counter + 1);
+			} else {
+				firstFacts.put(first, 1);
+			}
+			firstFacts.put(second, 0);
+		}
+		return firstFacts;
+	}
+
+	private boolean containsDisconnectedNodes(Episode episode) {
+		Set<Fact> relFacts = Sets.newHashSet();
+
+		for (Fact fact : episode.getRelations()) {
+			Tuple<Fact, Fact> relation = fact.getRelationFacts();
+			relFacts.add(relation.getFirst());
+			relFacts.add(relation.getSecond());
+		}
+		if (relFacts.size() < episode.getNumEvents()) {
+			return true;
+		}
+		return false;
+	}
+
+	public APIUsagePattern transform(Episode episode, List<Event> mapping) {
+		APIUsagePattern pattern = new APIUsagePattern(episode.getFrequency(),
+				new HashSet<>());
+		Map<Integer, Node> factIdToNodeMap = new HashMap<>();
+		for (Fact fact : episode.getFacts()) {
+			if (!fact.isRelation()) {
+				Event event = mapping.get(fact.getFactID());
+				IMethodName method = event.getMethod();
+				MethodCallNode node = TransformerUtils.createCallNode(method);
+				pattern.addVertex(node);
+				factIdToNodeMap.put(fact.getFactID(), node);
+			} else {
+				Tuple<Fact, Fact> relation = fact.getRelationFacts();
+				Node sourceNode = factIdToNodeMap.get(relation.getFirst()
+						.getFactID());
+				Node targetNode = factIdToNodeMap.get(relation.getSecond()
+						.getFactID());
+				pattern.addEdge(sourceNode, targetNode, new OrderEdge(
+						sourceNode, targetNode));
+			}
+		}
+		return pattern;
+	}
 }
--- EpisodeMisuseDetector/src/test/java/cc/episodeMining/mudetect/EpisodesToPatternTransformerTest.java
+++ EpisodeMisuseDetector/src/test/java/cc/episodeMining/mudetect/EpisodesToPatternTransformerTest.java
@@ -6,6 +6,7 @@ import static de.tu_darmstadt.stg.mudetect.aug.matchers.NodeMatchers.methodCall;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.Arrays;
@@ -26,86 +27,124 @@ import cc.kave.episodes.model.Episode;
 import cc.kave.episodes.model.events.Event;
 import cc.kave.episodes.model.events.EventKind;
 import cc.kave.episodes.model.events.Fact;
+import de.tu_darmstadt.stg.mudetect.aug.model.actions.MethodCallNode;
 import de.tu_darmstadt.stg.mudetect.aug.model.patterns.APIUsagePattern;
 
 public class EpisodesToPatternTransformerTest {
 
-    @Test
-    public void transformsOneEventEpisode() {
-        Set<Episode> episodes = new HashSet<>(Collections.singletonList(
-                createEpisode(42, new Fact(0))
-        ));
-        List<Event> mapping = Collections.singletonList(
-                createMethodCallEvent(Names.newMethod("0M:[p:void] [i:Namespace.DeclaringType, a, 1].M()"))
-        );
+	@Test
+	public void transformsOneEventEpisode() {
+		Set<Episode> episodes = new HashSet<>(
+				Collections.singletonList(createEpisode(42, new Fact(0))));
+		List<Event> mapping = Collections
+				.singletonList(createMethodCallEvent(Names
+						.newMethod("0M:[p:void] [i:Namespace.DeclaringType, a, 1].M()")));
 
-        Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer().transform(episodes, mapping);
+		Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer()
+				.transform(episodes, mapping);
 
-        assertThat(patterns, hasSize(1));
-        assertThat(patterns.iterator().next(), hasNodes(methodCall("Namespace.DeclaringType", "M()")));
-    }
+		assertThat(patterns, hasSize(1));
+		assertThat(patterns.iterator().next(),
+				hasNodes(methodCall("Namespace.DeclaringType", "M()")));
+	}
 
-    @Test
-    public void transformsMultiEventEpisode() {
-        Fact fact0 = new Fact(0);
-        Fact fact1 = new Fact(1);
-        Set<Episode> episodes = new HashSet<>(Collections.singletonList(
-                createEpisode(1337, fact0, fact1, new Fact(fact0, fact1))
-        ));
-        List<Event> mapping = Arrays.asList(
-                createMethodCallEvent(Names.newMethod("0M:[p:void] [i:Namespace.DeclaringType, a, 1].M()")),
-                createMethodCallEvent(Names.newMethod("0M:[p:void] [i:Other.Type, a, 1].N()"))
-        );
+	@Test
+	public void transformsMultiEventEpisode() {
+		Fact fact0 = new Fact(0);
+		Fact fact1 = new Fact(1);
+		Set<Episode> episodes = new HashSet<>(
+				Collections.singletonList(createEpisode(1337, fact0, fact1,
+						new Fact(fact0, fact1))));
+		List<Event> mapping = Arrays
+				.asList(createMethodCallEvent(Names
+						.newMethod("0M:[p:void] [i:Namespace.DeclaringType, a, 1].M()")),
+						createMethodCallEvent(Names
+								.newMethod("0M:[p:void] [i:Other.Type, a, 1].N()")));
 
-        Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer().transform(episodes, mapping);
+		Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer()
+				.transform(episodes, mapping);
 
-        assertThat(patterns, hasSize(1));
-        assertThat(patterns.iterator().next(),
-                hasOrderEdge(methodCall("Namespace.DeclaringType", "M()"), methodCall("Other.Type", "N()")));
-    }
+		assertThat(patterns, hasSize(1));
+		assertThat(
+				patterns.iterator().next(),
+				hasOrderEdge(methodCall("Namespace.DeclaringType", "M()"),
+						methodCall("Other.Type", "N()")));
+	}
 
-    @Test
-    public void keepsEpisodeFrequency() {
-        Set<Episode> episodes = new HashSet<>(Collections.singletonList(
-                createEpisode(2345890)
-        ));
-        List<Event> mapping = Collections.emptyList();
+	@Test
+	public void transformsTwoDiscEventEpisode() {
+		Fact fact0 = new Fact(0);
+		Fact fact1 = new Fact(1);
+		Set<Episode> episodes = new HashSet<>(
+				Collections.singletonList(createEpisode(1337, fact0, fact1)));
+		List<Event> mapping = Arrays
+				.asList(createMethodCallEvent(Names
+						.newMethod("0M:[p:void] [i:Namespace.DeclaringType, a, 1].M()")),
+						createMethodCallEvent(Names
+								.newMethod("0M:[p:void] [i:Other.Type, a, 1].N()")));
 
-        Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer().transform(episodes, mapping);
+		Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer()
+				.transform(episodes, mapping);
 
-        assertThat(patterns, hasSize(1));
-        assertThat(patterns.iterator().next().getSupport(), is(2345890));
-    }
+		Episode episode1 = createEpisode(1337, fact0, fact1, new Fact(fact0,
+				fact1));
+		Episode episode2 = createEpisode(1337, fact0, fact1, new Fact(fact1,
+				fact0));
+		APIUsagePattern pattern1 = new EpisodesToPatternTransformer()
+				.transform(episode1, mapping);
+		APIUsagePattern pattern2 = new EpisodesToPatternTransformer()
+				.transform(episode2, mapping);
 
-    @Test
-    public void smokeTest() {
-        File data = new File(getClass().getResource("/episodes").getFile().replaceAll("%20", " "));
+		assertThat(patterns, hasSize(2));
+		assertTrue(patterns.contains(pattern1));
+		assertTrue(patterns.contains(pattern2));
+	}
 
-        Map<Integer, Set<Episode>> episodesByNumNodes = new EpisodeParser(data, new FileReader()).parser(1);
-        EventStreamIo streamIo = new EventStreamIo(data);
-        List<Event> mapping = streamIo.readMapping(1);
-        EpisodesToPatternTransformer transformer = new EpisodesToPatternTransformer();
+	@Test
+	public void keepsEpisodeFrequency() {
+		Set<Episode> episodes = new HashSet<>(
+				Collections.singletonList(createEpisode(2345890)));
+		List<Event> mapping = Collections.emptyList();
 
-        Set<APIUsagePattern> patterns = new HashSet<>();
-        for (Set<Episode> episodes : episodesByNumNodes.values()) {
-            patterns.addAll(transformer.transform(episodes, mapping));
-        }
+		Set<APIUsagePattern> patterns = new EpisodesToPatternTransformer()
+				.transform(episodes, mapping);
 
-        System.out.println("Converted " + patterns.size() + " episodes to patterns.");
-    }
+		assertThat(patterns, hasSize(1));
+		assertThat(patterns.iterator().next().getSupport(), is(2345890));
+	}
 
-    private Episode createEpisode(int frequency, Fact... facts) {
-        Episode episode = new Episode();
-        episode.setFrequency(frequency);
-        episode.addListOfFacts(Arrays.asList(facts));
-        return episode;
-    }
+	@Test
+	public void smokeTest() {
+		File data = new File(getClass().getResource("/episodes").getFile()
+				.replaceAll("%20", " "));
 
-    private Event createMethodCallEvent(IMethodName methodName) {
-        Event event = new Event();
-        event.setKind(EventKind.INVOCATION);
-        event.setMethod(methodName);
-        return event;
-    }
+		Map<Integer, Set<Episode>> episodesByNumNodes = new EpisodeParser(data,
+				new FileReader()).parser(1);
+		EventStreamIo streamIo = new EventStreamIo(data);
+		List<Event> mapping = streamIo.readMapping(1);
+		EpisodesToPatternTransformer transformer = new EpisodesToPatternTransformer();
+
+		Set<APIUsagePattern> patterns = new HashSet<>();
+		for (Set<Episode> episodes : episodesByNumNodes.values()) {
+			patterns.addAll(transformer.transform(episodes, mapping));
+		}
+
+		System.out.println("Converted " + patterns.size()
+				+ " episodes to patterns.");
+	}
+
+	private Episode createEpisode(int frequency, Fact... facts) {
+		Episode episode = new Episode();
+		episode.setFrequency(frequency);
+		episode.addListOfFacts(Arrays.asList(facts));
+		return episode;
+	}
+
+	private Event createMethodCallEvent(IMethodName methodName) {
+		Event event = new Event();
+		event.setKind(EventKind.INVOCATION);
+		event.setMethod(methodName);
+		return event;
+	}
 
 }
