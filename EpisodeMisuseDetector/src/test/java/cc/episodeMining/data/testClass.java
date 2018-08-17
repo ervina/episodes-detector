3470189e35a286f746d07e28258a5eb5492492ca
ervina <ervinacergani@pc-cergani.st.informatik.tu-darmstadt.de>
2018-08-17 17:12:18 +0200

    added tests for SequenceGenerator

---
+++ EpisodeMisuseDetector/src/test/java/cc/episodeMining/data/testClass.java
@@ -0,0 +1,50 @@
+package cc.episodeMining.data;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+//class TestClass extends StrBuilder {
+//
+//	String pattern(Object obj) {
+//
+//		String str = (obj == null ? this.getNullText() : obj.toString());
+//		if (str == null) {
+//			str = "";
+//		}
+//		return str;
+//	}
+//}
+//
+//abstract class StrBuilder {
+//	String pattern(Object obj) {
+//		return null;
+//	}
+//
+//	String getNullText() {
+//		return "";
+//	}
+//}
+
+class TestClass implements StrBuilder {
+
+	public String pattern(Object obj) {
+
+		String str = (obj == null ? this.getNullText() : obj.toString());
+		if (str == null) {
+			List<Integer> someList = Lists.newLinkedList();
+			str = "";
+		}
+		return str;
+	}
+
+	public String getNullText() {
+		return "";
+	}
+}
+
+interface StrBuilder {
+	String pattern(Object obj);
+
+	String getNullText();
+}
